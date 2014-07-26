ae5d7f4533ecd14917f46c0ca4d520707d3d0823
chappaPraveen <praveenchappa2050@gmail.com>
2014-07-27 01:16:24 +0530

    index

--- employer.php
+++ employer.php
@@ -4,15 +4,90 @@
 <head> 
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script> 
     <script src="http://malsup.github.com/jquery.form.js"></script> 
- 
+    <meta name="description" content="website description" />
+   <meta name="keywords" content="website keywords, website keywords" />
+   <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
+   <link rel="stylesheet" type="text/css" href="style/style.css" title="style" />
+   <link rel="stylesheet" type="text/css" href="css/demo.css" />
+   <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/Custom.css" />
+   <script type="text/javascript" src="jquery.min.js"></script>
+<script type="text/javascript" src="jquery.form.js"></script>
     <script> 
         // wait for the DOM to be loaded 
        
     </script> 
+    <style> 
+ inputs:-webkit-input-placeholder { 
+    color:    #2E9AFE; 
+} 
+ 
+inputs-moz-placeholder { 
+    color:    #2E9AFE; 
+} 
+ 
+ .inputs  { 
+ width:200px; 
+padding: 8px 8px; 
+font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif; 
+font-weight: 400; 
+font-size: px; 
+color: #fffff; 
+
+background: #FFF; 
+border: 1px solid #FFF; 
+border-radius: 5px; 
+box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.50); 
+-moz-box-shadow: inset 0 1px 3px rgba(0,0,0,0.50); 
+-webkit-box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.50); 
+} 
+.inputs:focus { 
+   background: ##0101DF; 
+color: #FFFFFF; 
+box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.25); 
+-moz-box-shadow: inset 0 1px 2px rgba(0,0,0,0.25); 
+-webkit-box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.25); 
+    outline:0; 
+} 
+  .inputs:hover   { 
+background: #2E9AFE; 
+color: #FFFFFF; 
+} 
+.img { max-width: 100%; }
+ 
+ </style> 
 </head> 
 <body>
+<div id="main">
+    <div id="header">
+      <div id="logo">
+        <div id="logo_text">
+         <form action="logout" style="position:fixed;left:1200px;top:60px"><input type="submit" class="logout" value="Logout"v></form>
+          <!-- class="logo_colour", allows you to change the colour of the text -->
+          <h1><a href="index.html">Quest Connect<span class="logo_colour">Book</span></a></h1>
+          <h2></h2>
+        </div>
+      </div>
+      <div id="menubar">
+        <ul id="menu">
+          <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+           <li ><a href="">Home</a></li>
+          <li><a href="">Post aJob</a></li>
+               <li><a href="">Review contacts</a></li>
+          <li><a href="Team.j sp">Manage Posts</a></li>
+          <li class="selected"><a href="employer.php">Post a Job</a></li>
+        </ul>
+      </div>
+    </div>
 <div>
-<form action="employer.php" method="post">
+<div id="site_content">
+      <div class="sidebar">
+        <!-- insert your sidebar items here -->
+        
+        <h4>&nbsp;</h4>
+      </div>
+      <div id="content" style="height:1000px;width:565px;">
+        <form action="employer.php" method="post">
 <table cellpadding="1"   align="center" cellspacing="1" width="500px"  bgcolor="#3399FF" style="color:#fff" id="table1_hide">
 
 <tr colspan="2">
@@ -62,11 +137,21 @@
 </tr>
 </table>
 </font>
-</form> 
-
+</form>   <br>  <br>	
+      	<div id="accordion" style="overflow-y: scroll;">
+      	
+      	</div>
+        <p>&nbsp;</p>
 </div>
+    </div>
+    <div id="footer">
+        Copyrights@Engineering Note Book
+    </div>
+  </div>
 </body>
 </html>
+
+
 <?php
 $host="localhost";
 $usr="root";

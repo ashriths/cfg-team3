e684f47be475d3ddc974da9f8b356e0a6821c73d
chappaPraveen <praveenchappa2050@gmail.com>
2014-07-27 00:16:24 +0530

    database

--- jpmg.sql
+++ jpmg.sql
@@ -1,219 +1,35 @@
--- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jul 26, 2014 at 01:55 PM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.0.27-community-nt - MySQL Community Edition (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             8.1.0.4545
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- Data exporting was unselected.
 
---
--- Database: `jpmg`
---
+-- Data exporting was unselected.
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `area`
---
+-- Data exporting was unselected.
 
-CREATE TABLE IF NOT EXISTS `area` (
-  `area_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `city_id` int(11) NOT NULL,
-  PRIMARY KEY (`area_id`),
-  KEY `city_id` (`city_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+-- Data exporting was unselected.
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `city`
---
+-- Data exporting was unselected.
 
-CREATE TABLE IF NOT EXISTS `city` (
-  `city_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `state_id` int(11) NOT NULL,
-  PRIMARY KEY (`city_id`),
-  KEY `state_id` (`state_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+-- Data exporting was unselected.
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `country`
---
+-- Data exporting was unselected.
 
-CREATE TABLE IF NOT EXISTS `country` (
-  `country_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`country_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employer`
---
-
-CREATE TABLE IF NOT EXISTS `employer` (
-  `emp_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(300) NOT NULL,
-  `area_id` int(11) NOT NULL,
-  PRIMARY KEY (`emp_id`),
-  KEY `area_id` (`area_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `job`
---
-
-CREATE TABLE IF NOT EXISTS `job` (
-  `j_id` int(11) NOT NULL AUTO_INCREMENT,
-  `emp_id` int(11) NOT NULL,
-  `vacancies` int(11) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  PRIMARY KEY (`j_id`),
-  KEY `emp_id` (`emp_id`,`vacancies`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `job_skill`
---
-
-CREATE TABLE IF NOT EXISTS `job_skill` (
-  `j_id` int(11) NOT NULL,
-  `skill_id` int(11) NOT NULL,
-  KEY `j_id` (`j_id`,`skill_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `po`
---
-
-CREATE TABLE IF NOT EXISTS `po` (
-  `po_id` int(11) NOT NULL AUTO_INCREMENT,
-  `area_id` int(11) NOT NULL,
-  `name` varchar(300) NOT NULL,
-  PRIMARY KEY (`po_id`),
-  KEY `area_id` (`area_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `skill`
---
-
-CREATE TABLE IF NOT EXISTS `skill` (
-  `skill_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  PRIMARY KEY (`skill_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `state`
---
-
-CREATE TABLE IF NOT EXISTS `state` (
-  `state_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `country_id` int(11) NOT NULL,
-  PRIMARY KEY (`state_id`),
-  KEY `country_id` (`country_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `trainee`
---
-
-CREATE TABLE IF NOT EXISTS `trainee` (
-  `trainee_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` int(11) NOT NULL,
-  PRIMARY KEY (`trainee_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `trainee_skill`
---
-
-CREATE TABLE IF NOT EXISTS `trainee_skill` (
-  `trainee_id` int(11) NOT NULL,
-  `skill_id` int(11) NOT NULL,
-  KEY `trainee_id` (`trainee_id`,`skill_id`),
-  KEY `skill_id` (`skill_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `area`
---
-ALTER TABLE `area`
-  ADD CONSTRAINT `area_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`);
-
---
--- Constraints for table `city`
---
-ALTER TABLE `city`
-  ADD CONSTRAINT `city_ibfk_1` FOREIGN KEY (`state_id`) REFERENCES `state` (`state_id`);
-
---
--- Constraints for table `employer`
---
-ALTER TABLE `employer`
-  ADD CONSTRAINT `employer_ibfk_2` FOREIGN KEY (`emp_id`) REFERENCES `job` (`emp_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `employer_ibfk_1` FOREIGN KEY (`area_id`) REFERENCES `area` (`area_id`);
-
---
--- Constraints for table `job_skill`
---
-ALTER TABLE `job_skill`
-  ADD CONSTRAINT `job_skill_ibfk_1` FOREIGN KEY (`j_id`) REFERENCES `job` (`j_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `po`
---
-ALTER TABLE `po`
-  ADD CONSTRAINT `po_ibfk_1` FOREIGN KEY (`area_id`) REFERENCES `area` (`area_id`);
-
---
--- Constraints for table `state`
---
-ALTER TABLE `state`
-  ADD CONSTRAINT `state_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`);
-
---
--- Constraints for table `trainee_skill`
---
-ALTER TABLE `trainee_skill`
-  ADD CONSTRAINT `trainee_skill_ibfk_2` FOREIGN KEY (`skill_id`) REFERENCES `skill` (`skill_id`),
-  ADD CONSTRAINT `trainee_skill_ibfk_1` FOREIGN KEY (`trainee_id`) REFERENCES `trainee` (`trainee_id`);
-
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
